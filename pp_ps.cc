84f4b68366cf57a229a00ff226a8dbb66643539f
MagikarpUsedFly <wr140815@ohio.edu>
2019-02-20 09:01:38 -0500

    correctly allows imputs for displaying <command>

--- pp_ps.cc
+++ pp_ps.cc
@@ -7,10 +7,16 @@
 //******************************************************************************
 
 #include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <string>
 #include <fstream>
 #include <stdlib.h>
 #include <dirent.h>
 #include <vector>
+#include <math.h>
+#include <unistd.h>
+#include <iomanip>
 using namespace std;
 
 int const PROC_TYPE           = 4;
@@ -21,33 +27,88 @@ int const ARG_POS             = 1;
 
 //******************************************************************************
 // Struct:        PID
-// Traits:        u_int     pid, process ID
-//                string    cmd, command
-//                char      state, State
-//                float     cpu, CPU percentage
-//                float     mem, MEM percentage
-//                u_long    VSZ, Virtural memory size
-//                u_long    RSS, Resident set size
-//                u_int     cpu_exec, Last excuted
+// Traits:        int u       pid, process ID
+//                string      cmd, command
+//                char        state, State
+//                float       cpu, CPU percentage
+//                float       mem, MEM percentage
+//                long u      VSZ, Virtural memory size
+//                long u      RSS, Resident set size
+//                int u       cpu_exec, Last excuted
+//                long u      stime, process since kernal mode
+//                long long u uptime, time scheduled in user mode
+//                long long u starttime, time process after system boot
 // Description:   Structure to represent the /proc/pid/---- info
 //******************************************************************************
 struct PID {
-  u_int pid;
+  unsigned long pid;
   // Command
   string cmd;
   // State
   char state;
   // % Cpu
-  float cpu;
+  long double cpu;
   // % Memory
   float mem;
   // Virtural memory size
-  u_long VSZ;
+  unsigned long long int VSZ;
   // Resident set size
-  u_long RSS;
+  unsigned long long int RSS;
   // Cpu last excuted
-  u_int cpu_exec;
+  unsigned int cpu_exec;
+  // Amount of time that this process has been scheduled in kernal mode
+  unsigned long int stime;
+  // Amount of time that this process has been secheduled in user mode
+  unsigned long int utime;
+  // Time porcess started after system boot
+  unsigned long long int starttime;
 };
+
+
+//******************************************************************************
+// Function:      compare_cpu()
+// Arguments:     const PID (struct) x
+//                const PID (struct) y
+// Description:   Compares passed in struct.cpu values
+//******************************************************************************
+bool compare_cpu(const PID &x, const PID &y)
+{
+  return x.cpu > y.cpu;
+}
+
+//******************************************************************************
+// Function:      compare_mem()
+// Arguments:     const PID (struct) x
+//                const PID (struct) y
+// Description:   Compares passed in struct.mem values
+//******************************************************************************
+bool compare_mem(const PID &x, const PID &y)
+{
+  return x.mem > y.mem;
+}
+
+//******************************************************************************
+// Function:      compare_pid()
+// Arguments:     const PID (struct) x
+//                const PID (struct) y
+// Description:   Compares passed in struct.pid values
+//******************************************************************************
+bool compare_pid(const PID &x, const PID &y)
+{
+  return x.pid < y.pid;
+}
+
+//******************************************************************************
+// Function:      compare_cmd()
+// Arguments:     const PID (struct) x
+//                const PID (struct) y
+// Description:   Compares passed in struct.cmd values
+//******************************************************************************
+bool compare_cmd(const PID &x, const PID &y)
+{
+  return x.cmd < y.cmd;
+}
+
 
 
 //******************************************************************************
@@ -66,8 +127,18 @@ int get_cmd(int argc, char** argv)
 {
   string cmd;
   int cmd_code;
+
+  // Checking if passing exactly one command
+  if(argc != MAX_ARGC_COUNT)
+  {
+    fprintf(stderr, "Error: ./pp_ps <command>: No argument '%d'\n", argc);
+    exit(-1);
+  }
+
+  // Setting cmd value
   cmd = argv[ARG_POS];
 
+  // Set code value corosponding to cmd passed
   if(cmd == "-cpu")
     {cmd_code = 1;}
   else if(cmd == "-mem")
@@ -86,6 +157,152 @@ int get_cmd(int argc, char** argv)
 }
 
 
+//******************************************************************************
+// Function:      parse_line()
+// Arguments:     string line_buffer
+// Description:   Parses the /proc/<pid>/stat info and then inserts corrosponding
+//                struct attributes. Returning the struct itself when finished
+//******************************************************************************
+PID parse_line(string line_buffer)
+{
+    int index = 1;
+    PID tmp_pid;
+    string tmp;
+    stringstream iss(line_buffer);
+    while(iss >> tmp)
+    {
+      //cout << tmp << endl;
+      if(index == 1)
+      {
+        tmp_pid.pid = stoul(tmp);
+        //cout << tmp_pid.pid << endl;
+      }
+      else if(index == 2)
+      {
+        if( tmp.front() == '(' && tmp.back() != ')')
+        {
+          string tmp2;
+          const int lim = 1000;
+          int lim_counter = 0;
+          char tmp_char = ' ';
+
+          while(tmp_char != ')' && lim_counter < lim)
+          {
+            tmp2 = tmp;
+            iss >> tmp;
+            tmp = tmp2 + tmp;
+            lim_counter++;
+            tmp_char = tmp.back();
+          }
+          if(lim_counter >= lim) {perror("command name was invalid\n");}
+        }
+        tmp.erase(remove(tmp.begin(), tmp.end(), ')'), tmp.end());
+        tmp.erase(remove(tmp.begin(), tmp.end(), '('), tmp.end());
+        tmp_pid.cmd = tmp;
+        //cout << tmp_pid.cmd << endl;
+      }
+      else if(index == 3)
+      {
+        tmp_pid.state = tmp[0];
+        //cout << tmp_pid.state << endl;
+      }
+      else if(index == 14)
+      {
+        tmp_pid.utime = stoul(tmp);
+        //cout << tmp_pid.utime << endl;
+      }
+      else if(index == 15)
+      {
+        tmp_pid.stime = stoul(tmp);
+        //cout << tmp_pid.stime << endl;
+      }
+      else if(index == 22)
+      {
+        tmp_pid.starttime = stoull(tmp);
+        //cout << tmp_pid.starttime << endl;
+      }
+      else if(index == 23)
+      {
+        tmp_pid.VSZ = stoull(tmp);
+        //cout << tmp_pid.VSZ << endl;
+      }
+      else if(index == 24){
+        tmp_pid.RSS = stoull(tmp);
+        //cout << tmp_pid.RSS << endl;
+      }
+      else if(index == 39)
+      {
+        tmp_pid.cpu_exec = stoull(tmp);
+        //cout << tmp_pid.cpu_exec << endl;
+      }
+      index++;
+    }
+    return tmp_pid;
+}
+
+void display(vector<PID> p_pid, int cmd_code)
+{
+  const int LINES_OUTPUT = 50;
+
+  system("tabs 19");
+  cout << fixed;
+  cout << setprecision(6);
+
+  if(cmd_code == 1){sort(p_pid.begin(), p_pid.end(), compare_cpu);}
+  else if(cmd_code == 2){sort(p_pid.begin(), p_pid.end(), compare_mem);}
+  else if(cmd_code == 3){sort(p_pid.begin(), p_pid.end(), compare_pid);}
+  else if(cmd_code == 4){sort(p_pid.begin(), p_pid.end(), compare_cmd);}
+
+  for(int i = 0; i < p_pid.size(); i++)
+  {
+    cout << p_pid[i].pid << "\t" << p_pid[i].cmd << "\t" << p_pid[i].state <<
+    "   " << p_pid[i].cpu << "\t" << p_pid[i].mem << "\t" << p_pid[i].VSZ <<
+    "\t" << p_pid[i].RSS << "\t" << p_pid[i].cpu_exec << "\n";
+  }
+
+/*
+  if(cmd_code == 1)
+  {
+    sort(p_pid.begin(), p_pid.end(), compare_cpu);
+    for(int i = 0; i < p_pid.size(); i++)
+    {
+      cout << p_pid[i].pid << "\t" << p_pid[i].cmd << "\t" << p_pid[i].state <<
+      "   " << p_pid[i].cpu << "\t" << p_pid[i].mem << "\t" << p_pid[i].VSZ <<
+      "\t" << p_pid[i].RSS << "\t" << p_pid[i].cpu_exec << "\n";
+    }
+  }
+  if(cmd_code == 2)
+  {
+    sort(p_pid.begin(), p_pid.end(), compare_mem);
+    for(int i = 0; i < p_pid.size(); i++)
+    {
+      cout << p_pid[i].pid << "\t" << p_pid[i].cmd << "\t" << p_pid[i].state <<
+      "   " << p_pid[i].cpu << "\t" << p_pid[i].mem << "\t" << p_pid[i].VSZ <<
+      "\t" << p_pid[i].RSS << "\t" << p_pid[i].cpu_exec << "\n";
+    }
+  }
+  if(cmd_code == 3)
+  {
+    sort(p_pid.begin(), p_pid.end(), compare_pid);
+    for(int i = 0; i < p_pid.size(); i++)
+    {
+      cout << p_pid[i].pid << "\t" << p_pid[i].cmd << "\t" << p_pid[i].state <<
+      "   " << p_pid[i].cpu << "\t" << p_pid[i].mem << "\t" << p_pid[i].VSZ <<
+      "\t" << p_pid[i].RSS << "\t" << p_pid[i].cpu_exec << "\n";
+    }
+  }
+  if(cmd_code == 4)
+  {
+    sort(p_pid.begin(), p_pid.end(), compare_cmd);
+    for(int i = 0; i < p_pid.size(); i++)
+    {
+      cout << p_pid[i].pid << "\t" << p_pid[i].cmd << "\t" << p_pid[i].state <<
+      "   " << p_pid[i].cpu << "\t" << p_pid[i].mem << "\t" << p_pid[i].VSZ <<
+      "\t" << p_pid[i].RSS << "\t" << p_pid[i].cpu_exec << "\n";
+    }
+  }
+  */
+}
 
 //******************************************************************************
 // Function:      main()
@@ -95,11 +312,12 @@ int get_cmd(int argc, char** argv)
 //                kappa
 //******************************************************************************
 int main(int argc, char** argv) {
-
   // Nessary Variables
-  string PROC           = "/proc";
-  string cmd            = "";
-  int cmd_code          = 0;
+  string PROC             = "/proc";
+  string uptime           = "uptime";
+  long double uptime_val  = 0;
+  string cmd              = "";
+  int cmd_code            = 0;
   int pid;
   int read;
 
@@ -111,6 +329,10 @@ int main(int argc, char** argv) {
   DIR           *dir;
   FILE          *fp;
 
+  // Available mem
+  long phys_pages = sysconf(_SC_PHYS_PAGES);
+  long page_size = sysconf(_SC_PAGE_SIZE);
+  long phys_mem_size = phys_pages * page_size;
 
   cmd_code = get_cmd(argc, argv);
   dir = opendir ("/proc");
@@ -119,6 +341,32 @@ int main(int argc, char** argv) {
     // print all the pid's within /proc
     while ((ent = readdir (dir)) != NULL)
     {
+      // Directory ed->d_name
+      //cout << ent -> d_name << endl;
+      if(uptime.compare(ent -> d_name) == 0)
+      {
+        char tmp[255];
+        char* line_buffer = NULL;
+        size_t  blength;
+        sprintf(tmp, "/proc/%s", "uptime");
+        fp = fopen(tmp, "r");
+        getline(&line_buffer, &blength, fp);
+        int i = 1;
+        string tmp_;
+        istringstream iss(line_buffer);
+
+        while(iss){
+          string tmp_;
+          iss >> tmp_;
+          if(i == 1)
+          {
+            //cout << tmp_ << endl;
+            uptime_val = stold(tmp_);
+          }
+          i++;
+        }
+        fclose(fp);
+      }
       pid = atoi(ent -> d_name);
       if((ent -> d_type == PROC_TYPE) && (pid > 0))
       {
@@ -137,13 +385,25 @@ int main(int argc, char** argv) {
         }
         while( (read = getline(&line_buffer, &blength, fp)) != -1)
         {
-          printf("Line: %s\n", line_buffer);
+          //printf("Line: %s\n", line_buffer);
+          PID process;
+          process = parse_line(line_buffer);
+
+          process.mem = ((process.RSS * getpagesize() * 100.0) / phys_mem_size);
+          long double real_time;
+          real_time = uptime_val - (process.starttime/sysconf(_SC_CLK_TCK));
+          long double process_time = (process.utime / sysconf(_SC_CLK_TCK) ) + ( process.stime / sysconf(_SC_CLK_TCK) );
+          process.cpu = process_time * 100.0 /real_time;
+
+          //printf("%.6f\n", process.mem);
+          //cout << process.cpu << endl;
+          //printf("%Le\n", process.cpu);
+          p_pid.push_back(process);
           free(line_buffer);
         }
-        //cout << "pid " << pid << endl;
-        //printf ("%s\n", ent->d_name
       }
     }
+    display(p_pid, cmd_code);
     closedir (dir);
   }
   else
@@ -152,6 +412,8 @@ int main(int argc, char** argv) {
     perror("Error: Unable to open directory");
     exit(-1);
   }
+
+  //display();
 
   return 0;
 }
